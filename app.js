85adac8dec4de8d6f27ea507a30630ef959f5e49
wentingliu <cindy810317@gmail.com>
2021-11-07 16:30:37 +0800

    add relations between user and todo models

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const app = express()
 const PORT = 3000
 
@@ -25,7 +29,9 @@ app.get('/users/register', (req, res) => {
   res.render('register')
 })
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))gi
 })
 app.get('/users/logout', (req, res) => {
   res.send('logout')
---
+++ migrations/20211107081310-add-userId-to-todo.js
@@ -0,0 +1,18 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Add altering commands here.
+    return queryInterface.addColumn('Todos', 'UserId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    // Add reverting commands here.
+    return queryInterface.removeColumn('Todos', 'UserId')
+  }
+}
